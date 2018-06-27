341e023d2d6253e1a556939a74e111c7d407ee79
178031608 <178031608@qq.com>
2018-06-27 14:09:39 +0800

    2018-6-27 1409

--- fruitday/index/views.py
+++ fruitday/index/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -9,12 +10,21 @@ def login_views(request):
 
 def register_views(request):
 	if request.method == 'POST':
+		#有胡输入的手机
 		phone = request.POST['zphone']
+		#用户输入的密码
+		pwd = request.POST['zpwd']
+		#用户输入的名字
+		name=request.POST['uname']
+		#用户输入的邮箱
+		email = request.POST['uemail']
 		phones = Users.objects.all()
 		for phon in phones:
 			if phone == phon.uphone:
 				sms = '手机号已被注册'
 			else:
-				pass
+				obj = Users(uphone=phone,upwd=pwd,uname=name,uemail=email)
+				obj.save()
+			return HttpResponse('OK')
 		
 	return render(request, 'register.html',locals())
