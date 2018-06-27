e8f3e1ab8bab4350691110b5e79fc12d204b55fb
178031608 <178031608@qq.com>
2018-06-27 13:54:54 +0800

    2018-6-27 1200

--- fruitday/index/static/js/register.js
+++ fruitday/index/static/js/register.js
@@ -1,5 +1,14 @@
 $(function () {
     $('frmList').submit(function(){
+        var zphone = $('[name = zphone]').val();
+        var zpwd = $('[name = zpwd]').val();
+        var zpwd1 = $('[name = zpwd1]').val();
+        var zname = $('[name = zname]').val();
+        var zemail = $('[name = zemail]').val();
+       if(zphone.length==0 ||zpwd.length==0 ||zpwd1.length==0 ||zname.length==0 ||zemail.length==0 ){
+           return false;
 
+       }
+       return true;
     });
 });
--- fruitday/index/views.py
+++ fruitday/index/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import *
 
 # Create your views here.
 
@@ -8,4 +8,13 @@ def login_views(request):
 
 
 def register_views(request):
-	return render(request, 'register.html')
+	if request.method == 'POST':
+		phone = request.POST['zphone']
+		phones = Users.objects.all()
+		for phon in phones:
+			if phone == phon.uphone:
+				sms = '手机号已被注册'
+			else:
+				pass
+		
+	return render(request, 'register.html',locals())
